8eaece331f932dd74a950756328bea32248ff3a6
Stanislav Avramenko <91852555+Avramenko85@users.noreply.github.com>
2022-11-24 22:31:07 +0200

    Add files via upload

---
+++ second hw/js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// ===== task #1 ===========
+
+let name = 'John';
+let admin = name;
+alert(admin);
+
+// ====== task #2 ==========
+
+let earth;
+let userName;
+
+// ======= task #3 =========
+
+let numbers = '10' + 10;
+console.log(typeof numbers);
