bfc4427b6da6f0aac693fde55ecf7aa8a951a328
Stanislav Avramenko <91852555+Avramenko85@users.noreply.github.com>
2022-11-24 23:34:50 +0200

    Add files via upload

---
+++ third hw/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let firstOperand = prompt('Вкажіть число!');
+
+let secondOperand = prompt('Тепер друге!');
+
+// let result = +firstOperand + +secondOperand;
+
+alert(+firstOperand + +secondOperand);
+alert(firstOperand - secondOperand);
+alert(firstOperand / secondOperand);
+alert(firstOperand * secondOperand);
