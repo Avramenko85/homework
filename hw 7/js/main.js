79928ff0f10b82a37ae3b7bb2300066739cd8997
Stanislav Avramenko <91852555+Avramenko85@users.noreply.github.com>
2022-12-15 09:10:37 +0200

    Add files via upload

---
+++ hw 7/js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+let NumberOfHours = +prompt('Вкажіть кількість годин!!');
+
+const hour = (isNaN(NumberOfHours)) ? alert('введіть число') : alert(`В ${NumberOfHours} годинах ${NumberOfHours2 * 3600} секунд!!!`);
+
+
+// if (isNaN(NumberOfHours)) {
+//     alert('введіть число')
+// } else  {
+//     alert(`В ${NumberOfHours} годинах ${NumberOfHours * 3600} секунд!!!`);
+// };
