d0372a659ee99fca7b1fb1baad28910e0b1f4859
Stanislav Avramenko <91852555+Avramenko85@users.noreply.github.com>
2022-12-19 21:09:52 +0200

    Add files via upload

---
+++ hw 13/js/main.js
@@ -0,0 +1,72 @@
+'use strict';
+
+// ----------Task 1-----------------
+
+// let x = 10;
+// let y = 7;
+
+// let a = x > y ? 'x' : 'y';
+// console.log(a);
+
+// -----------Task 2------------
+
+// let userNum = +prompt('введіть число');
+
+// if (userNum % 2 === 0) {
+//     alert(`Число ${userNum} парне!`);
+// }  else {
+//     alert(`Парне ${userNum} не парне`);
+// }
+
+// -------------Task 3 ------------------
+
+
+// let userNum2 = prompt('Введите число');
+
+//  if (userNum2 > 0 && userNum2 < 10) {
+//   alert( `Число ${userNum2} однозначне позитивне` );
+// } else if (userNum2 >= 10 && userNum2 < 100) {
+//   alert( `Число ${userNum2} двозначне позитивне` );
+// } else if (userNum2 >= 100) {
+//   alert( `Число ${userNum2} трьохзначне позитивне` );
+// } else if (userNum2 < 0 && userNum2 > -10) {
+//   alert( `Число ${userNum2} однозначне негативне` );
+// } else if (userNum2 <= -10 && userNum2 > -100) {
+//   alert( `Число ${userNum2} двозначне негативне` );
+// } else {
+//   alert( `Число ${userNum2} трьохзначне негативне` );
+// }
+
+//-------------- task 4-------------------
+
+// let num = +prompt('number1');
+// let num1 = +prompt('number2');
+// let num2 = +prompt('number3');
+
+// if (num > num1 && num > num2) {
+//     alert(`first ${num}`);
+// } else if (num1 > num && num1 > num2) {
+//     alert(`second ${num1}`);
+// } else if (num2 > num && num2 > num1) {
+//     alert(`third ${num2}`);
+// } else {
+//     alert('are the same');
+// }
+
+// ------------Task 5--------------
+
+// let triSide1 = +prompt('Введіть довжину першої сторони!');
+// let triSide2 = +prompt('Введіть довжину другої сторони!');
+// let triSide3 = +prompt('Введіть довжину третьої сторони!');
+
+// if (triSide1 > triSide2 + triSide3) {
+//     alert('Такий трикутник може існувати');
+// } else if (triSide2 > triSide1 + triSide3) {
+//     alert('Такий трикутник може існувати');
+// } else if (triSide3 > triSide1 + triSide2) {
+//     alert('Такий трикутник може існувати');
+// } else {
+//     alert('Трикутник не може існувати при таких параметрах');
+// }
+
+// console.log(triSide1, triSide2, triSide3);
