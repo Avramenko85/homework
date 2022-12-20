ca257451820b19a0b5e5cd4d0e4383fb0975d613
Stanislav Avramenko <91852555+Avramenko85@users.noreply.github.com>
2022-12-20 14:46:27 +0200

    Add files via upload

---
+++ hw 8/js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+//1
+let num = 1;
+
+//2
+num = num += 12;
+
+//3
+num = num -= 14;
+
+//4
+num = num *= 5;
+
+//5
+num = num /= 7;
+
+//6
+num = ++num ;
+
+//7
+num = --num;
+
+// result
+alert(num);
